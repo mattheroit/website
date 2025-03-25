5c62d3843b48ffd1dc4697b5d990edde477650d1
mattheroit <mattheroit@pm.me>
2025-03-25 14:32:39 +0100

    working CJLrozbory

--- .vitepress/scripts/create_sidebar_links.ts
+++ .vitepress/scripts/create_sidebar_links.ts
@@ -15,6 +15,44 @@ function extractFirstHeading(content: string): string {
 	return match ? match[1] : "Untitled";
 }
 
+// Filters out everything that isn't a directory or md files (exept for index.md)
+function filterFileNames(fileNames: fs.Dirent[]) {
+	return fileNames
+		.filter((entry: fs.Dirent) => entry.isDirectory() || entry.name.endsWith(".md"))
+		.filter((entry: fs.Dirent) => entry.name !== "index.md");
+}
+
+// Recursive function to read folders and files
+function getSidebarItems(dataDir: string, baseDir: string): any[] {
+	return filterFileNames(getFileNames(dataDir, true)).map((entry: fs.Dirent) => {
+		const fullPath = path.join(dataDir, entry.name);
+		const newBaseDir = `${baseDir}/${entry.name}`;
+		let subfolderItems: any[] = [];
+		let text: string | null = null;
+		let link: string | null = null;
+
+		if (entry.isDirectory()) {
+			subfolderItems = getSidebarItems(fullPath, newBaseDir) || [];
+
+			// Check if this folder has an index.md
+			const indexPath = path.join(fullPath, "index.md");
+			if (fs.existsSync(indexPath)) {
+				text = extractFirstHeading(readFile(indexPath).content);
+				link = `${newBaseDir}/`;
+			}
+		} else {
+			text = extractFirstHeading(readFile(fullPath).content);
+			link = newBaseDir;
+		}
+
+		return {
+			text: text || entry.name,
+			...(link ? { link: link } : {}),
+			...(subfolderItems.length > 0 ? { collapsed: false, items: subfolderItems } : {}),
+		};
+	});
+}
+
 export function CJLrozbory() {
 	const dataDir: string = path.resolve(__dirname, "../../src/CJL/rozbory");
 	return filterRozbory(getFileNames(dataDir)).map((filename: string) => {
@@ -26,7 +64,7 @@ export function CJLrozbory() {
 	});
 }
 
-// TODO
 export function CJLzapisky() {
-	return [{ text: "", link: "" }];
+	const baseDir: string = "/CJL/zapisky";
+	return getSidebarItems(getDataDir(baseDir), baseDir);
 }
